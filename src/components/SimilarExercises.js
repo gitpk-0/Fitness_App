effae480221bd916bcecf67df6dc06cda1c274b2
gitpk-0 <kell108p@protonmail.com>
2022-11-01 13:28:25 -0400

    readme photo

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -7,7 +7,7 @@ import Loader from "./Loader";
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
-      <Typography variant="h3" mb={5}>
+      <Typography variant="h3" mb={5} mt={5}>
         Exercises that target the same muscle group
       </Typography>
       <Stack direction="row" sx={{ p: "2", position: "relative" }}>
@@ -17,7 +17,7 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
           <Loader />
         )}
       </Stack>
-      <Typography variant="h3" mb={5}>
+      <Typography variant="h3" mb={5} mt={5}>
         Exercises that use the same equipment
       </Typography>
       <Stack direction="row" sx={{ p: "2", position: "relative" }}>
